ed9eff590a71d0135753f91e3ba204845e9e4697
NutchapolSal <nutchapol.sal@gmail.com>
2025-01-26 16:13:33 +0700

    change guideline to be calculated using mod and abs

--- Guideline.cs
+++ Guideline.cs
@@ -43,8 +43,7 @@ public class Guideline : GameObject
 
     // 192 -> gameboard border
     // 24 -> ball radius
-    private float LeftBorder { get { return -192 + 24; } }
-    private float RightBorder { get { return +192 - 24; } }
+    private float HalfBoardWidth { get { return +192 - 24; } }
     public override void Draw(SpriteBatch spriteBatch, GameTime gameTime)
     {
         Debug.Assert(_texture is not null, "Guideline texture is not loaded");
@@ -55,37 +54,44 @@ public class Guideline : GameObject
         {
             var subProgress = (_progress + (float)i / _count) % 1.0f;
             var actualScale = new Vector2(3, 3) * (1.0f - subProgress);
-            var lengthLeft = _length * subProgress;
-            var tmpDirection = direction;
-            var s = new Vector2(0, 0);
-            Vector2 subPosition;
-            while (true)
-            {
-                var e = s + (tmpDirection * lengthLeft);
-                var slope = (e.Y - s.Y) / (e.X - s.X);
-                Vector2? bouncePoint = null;
+            var lengthFromSlingshot = _length * subProgress;
+            var vecToEndPos = direction * lengthFromSlingshot;
 
-                if (e.X > s.X && e.X > RightBorder)
-                {
-                    bouncePoint = new Vector2(RightBorder, slope * (RightBorder - s.X) + s.Y);
-                }
-                else if (e.X < s.X && e.X < LeftBorder)
-                {
-                    bouncePoint = new Vector2(LeftBorder, slope * (LeftBorder - s.X) + s.Y);
-                }
+            // exploiting modulo and absolute to do reflections
+            // general idea is to have the line "loop" over 2 board lengths, (modulo)
+            // then "fold" half of the board into itself (abs)
+            // most code is just moving the 0 point around
+            // so modulo and abs can do its thing
+            // see https://youtu.be/YZBg4M-MO8A
 
-                lengthLeft -= Vector2.Distance(s, bouncePoint ?? e);
-                s = bouncePoint ?? e;
-                tmpDirection = new Vector2(-tmpDirection.X, tmpDirection.Y);
-                if (bouncePoint is null || lengthLeft <= 0)
-                {
-                    subPosition = e;
-                    break;
-                }
-            }
+            // shift coordinates so
+            // left edge of "negative" board is 0,
+            // and right edge of "positive" board is 2 * Width
+            var x1 = vecToEndPos.X + 3 * HalfBoardWidth;
+
+            // loop coordinates so everything falls between 0 and 2 * Width
+            // compute modulo using remainder operator, handling case where x1 is negative
+            var x2 = ((x1 % (4 * HalfBoardWidth)) + 4 * HalfBoardWidth) % (4 * HalfBoardWidth);
+
+            // shift coordinates so
+            // left edge of "negative" board is -Width,
+            // center edge is 0,
+            // and right edge of "positive" board is Width
+            var x3 = x2 - 2 * HalfBoardWidth;
+
+            // reflect coordinates
+            // from the "negative" board onto the "positive" board
+            var x4 = Math.Abs(x3);
+
+            // shift coordinates so
+            // left and right edge of board is on ±0.5 * Width,
+            // and 0 in the center of the board
+            var x5 = x4 - HalfBoardWidth;
+
+            Vector2 calculatedPos = new(x5, vecToEndPos.Y);
             spriteBatch.Draw(
                 _texture,
-                subPosition + ScreenPosition,
+                calculatedPos + ScreenPosition,
                 null,
                 Color.White * (0.25f * (1.0f - subProgress)),
                 0,
