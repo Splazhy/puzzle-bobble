9ef4a58268524dd0cd6c0f961cc18de6c48176eb
NutchapolSal <Nutchapol.sal@gmail.com>
2025-02-03 14:48:57 +0700

    add logo

--- PuzzleBobble/Scene/MenuScene.cs
+++ PuzzleBobble/Scene/MenuScene.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
@@ -13,6 +14,8 @@ public class MenuScene : AbstractScene
     private Game? _game;
     private Desktop? _desktop;
 
+    private Texture2D? _logoTexture;
+
     public MenuScene() : base("scene_menu")
     {
     }
@@ -25,6 +28,7 @@ public class MenuScene : AbstractScene
     public override void LoadContent(ContentManager content)
     {
         base.LoadContent(content);
+        _logoTexture = content.Load<Texture2D>("Graphics/logo");
 
         Button startBtn = new()
 
@@ -92,6 +96,26 @@ public class MenuScene : AbstractScene
     public override Desktop? DrawMyra()
     {
         return _desktop;
+    }
+
+    public override void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+    {
+        Debug.Assert(_logoTexture is not null, "Logo texture is not loaded.");
+
+        var wave = (float)(Math.Sin(gameTime.TotalGameTime.TotalSeconds * Math.PI * 1.2) * 2);
+
+
+        spriteBatch.Draw(
+            _logoTexture,
+            ScreenPositionO(new Vector2(257, -140 + wave)),
+            null,
+            Color.White,
+            0,
+            new Vector2(_logoTexture.Width, 0),
+            PIXEL_SIZE * (2f / 3f),
+            SpriteEffects.None,
+            0
+        );
     }
 
 }
